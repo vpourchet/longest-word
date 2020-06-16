cbe26598c76af5a41b6aa2773d4f3bf84ff76cb7
Valentin Pourchet <valentin.pourchet@gmail.com>
2020-06-16 13:26:40 +0200

    Game development with TDD

---
+++ game.py
@@ -0,0 +1,19 @@
+import random
+import string
+
+class Game:
+    def __init__(self):
+        self.grid = []
+        for _ in range(9):
+            self.grid.append(random.choice(string.ascii_uppercase))
+
+    def is_valid(self, word):
+        if not word:
+            return False
+        letters = self.grid.copy()
+        for letter in word:
+            if letter in letters:
+                letters.remove(letter)
+            else:
+                return False
+        return True
